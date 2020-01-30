673ea9a3bb7ff5db8037bb761fed9c22670b60f6
modernmodem <60488313+modernmodem@users.noreply.github.com>
2020-01-30 15:21:34 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getThemes () {
     return {
-      'plugin-light-mode': {
+      'plugin-retro-mode': {
         cssPath: 'styles/light.css',
         darkMode: false
       }
